400c970c9acb2b7588682184092cc1df8aa086fe
Paulartn <if24b296@technikum-wien.at>
2026-05-24 19:49:20 +0200

    failing test for Testcase 2 already implemented -Senol

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -5,4 +5,8 @@ describe('Electrons around the cores', () => {
         expect(typeof calculateElectrons).toBe('function');
     });
 
+	it('should return 0 when dice have no electrons (2, 4, 6)', () => {
+        expect(calculateElectrons([2, 4, 6, 2])).toBe(0);
+    });
+
     });
