6dedcb2827eccb361e2355e45dd17112de946405
Paulartn <if24b296@technikum-wien.at>
2026-05-24 19:40:28 +0200

    Testcase 1 In Progress -Paul

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,25 +1,4 @@
-// StringCalculator.js
-function StringCalculator() {}
-StringCalculator.prototype.add = function(string_numbers) {
-	if(string_numbers === '')
-	{
-		return 0;
-	} 
-	else
-	{
-		let numbers = string_numbers.split(',');
-		let sum = 0;
-
-		for (let i = 0; i < numbers.length; i++)
-		{
-			let value = parseInt(numbers[i], 10);
-
-			if (!isNaN(value))
-			{
-				sum += value;
-			}
-		}
-		return sum;
-	}
-	
-};
+function calculateElectrons(diceArray) {
+    // Das ist die minimalste Implementierung für den ersten roten/grünen Test
+    return 0; 
+}
